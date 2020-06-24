0779ac531f2bb0661f1aacd6d62d357509f04266
Kittaka <jdavisson87@gmail.com>
2020-06-24 16:56:20 -0500

    Orders are only user Orders
    Adjusted get call to firebase to only retrieve orders that have the
    same userId as the current user.  This prevents seeing orders that
    the user did not make.

--- src/containers/Checkout/ContactData/ContactData.component.jsx
+++ src/containers/Checkout/ContactData/ContactData.component.jsx
@@ -109,6 +109,7 @@ class ContactData extends Component {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
       orderData: formData,
+      userId: this.props.userId,
     };
 
     this.props.onOrderBurger(order, this.props.token);
@@ -196,6 +197,7 @@ const mapStateToProps = (state) => {
     totalPrice: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
     token: state.auth.token,
+    userId: state.auth.userId,
   };
 };
 
