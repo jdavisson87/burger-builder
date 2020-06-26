32f95754969562ffde186441ef99f86ca75fa513
Kittaka <jdavisson87@gmail.com>
2020-06-26 11:17:48 -0500

    Testing <NavigationItems /> added one more test

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -13,11 +13,20 @@ describe('<NavigationItems />', () => {
   beforeEach(() => {
     wrapper = shallow(<NavigationItems />);
   });
+
   it('should render 2 <NavigationItems /> if un-authenticated', () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
+
   it('should render three <NavigationItem />  elements if authenticated', () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should render logout <NavigationItem />  element if authenticated', () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
   });
 });
