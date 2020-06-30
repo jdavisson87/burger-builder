681be3768770f99d50109bc3adb6fd0552488b0a
Kittaka <jdavisson87@gmail.com>
2020-06-30 14:44:06 -0500

    Testing BurgerBuilder container
    Testing Burger Builder container that it renders a BuildControls
    component after the burger builder container receives ingredients and
    an onInitIngredients prop

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -1,3 +1,21 @@
-test('Fake Test', () => {
-  expect(true).toBe(true);
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { BurgerBuilder } from './BurgerBuilder.container';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls.component';
+
+configure({ adapter: new Adapter() });
+
+describe('<BurgerBuilder />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<BurgerBuilder onInitIngredients={() => {}} />);
+  });
+
+  it('should render <BuildControls /> when receiving ingredients', () => {
+    wrapper.setProps({ ingredients: { salad: 0 } });
+    expect(wrapper.find(BuildControls)).toHaveLength(1);
+  });
 });
